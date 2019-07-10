258d0ca59143e79b8018f099f90744c1e95ac27e
HokieGeek <andres.f.perez@gmail.com>
2019-07-10 07:04:31 -0400

    [iq] Added a two new getters to retrieve a list of organizations and just a single one

--- iq/organization.go
+++ iq/organization.go
@@ -2,6 +2,7 @@ package nexusiq
 
 import (
 	"encoding/json"
+	"fmt"
 )
 
 const restOrganization = "api/v2/organizations"
@@ -10,10 +11,56 @@ type iqNewOrgRequest struct {
 	Name string `json:"name"`
 }
 
-type iqNewOrgResponse struct {
+type allOrgsResponse struct {
+	Organizations []Organization `json:"organizations"`
+}
+
+// Organization describes the data in IQ about a given organization
+type Organization struct {
 	ID   string   `json:"id"`
 	Name string   `json:"name"`
 	Tags []string `json:"tags"`
+}
+
+func (a *Organization) Equals(b *Organization) (_ bool) {
+	if a == b {
+		return true
+	}
+
+	if a.ID != b.ID {
+		return
+	}
+
+	if a.Name != b.Name {
+		return
+	}
+
+	if len(a.Tags) != len(b.Tags) {
+		return
+	}
+
+	for i, t := range a.Tags {
+		if t != b.Tags[i] {
+			return
+		}
+	}
+
+	return true
+}
+
+// GetOrganizationByName returns details on the named IQ organization
+func GetOrganizationByName(iq IQ, organizationName string) (*Organization, error) {
+	orgs, err := GetAllOrganizations(iq)
+	if err != nil {
+		return nil, err
+	}
+	for _, org := range orgs {
+		if org.Name == organizationName {
+			return &org, nil
+		}
+	}
+
+	return nil, fmt.Errorf("Did not find organization with name %s", organizationName)
 }
 
 // CreateOrganization creates an organization in IQ with the given name
@@ -28,10 +75,25 @@ func CreateOrganization(iq IQ, name string) (string, error) {
 		return "", err
 	}
 
-	var resp iqNewOrgResponse
-	if err = json.Unmarshal(body, &resp); err != nil {
+	var org Organization
+	if err = json.Unmarshal(body, &org); err != nil {
 		return "", err
 	}
 
-	return resp.ID, nil
+	return org.ID, nil
+}
+
+// GetAllOrganizations returns a slice of all of the organizations in an IQ instance
+func GetAllOrganizations(iq IQ) ([]Organization, error) {
+	body, _, err := iq.Get(restOrganization)
+	if err != nil {
+		return nil, err
+	}
+
+	var resp allOrgsResponse
+	if err = json.Unmarshal(body, &resp); err != nil {
+		return nil, err
+	}
+
+	return resp.Organizations, nil
 }
