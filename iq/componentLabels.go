5181a3ace19d8e73514b2a95bcf25a8836af7c2b
HokieGeek <andres.f.perez@gmail.com>
2019-07-31 16:08:34 -0400

    [iq] Added support for component labels

---
+++ iq/componentLabels.go
@@ -0,0 +1,41 @@
+package nexusiq
+
+import (
+	"fmt"
+	"net/http"
+)
+
+// api/v2/components/{componentHash}/labels/{labelName}/applications/{applicationId}
+const restLabelComponent = "api/v2/components/%s/labels/%s/applications/%s"
+
+// ComponentLabelApply adds an existing label to a component for a given application
+func ComponentLabelApply(iq IQ, label string, comp Component, appID string) error {
+	app, err := GetApplicationByPublicID(iq, appID)
+	if err != nil {
+		return fmt.Errorf("could not retrieve application with ID %s: %v", appID, err)
+	}
+
+	endpoint := fmt.Sprintf(restLabelComponent, comp.Hash, label, app.ID)
+	_, resp, err := iq.Post(endpoint, nil)
+	if err != nil && resp.StatusCode != http.StatusNoContent {
+		return fmt.Errorf("could not apply label: %v", err)
+	}
+
+	return nil
+}
+
+// ComponentLabelUnapply removes an existing association between a label and a component
+func ComponentLabelUnapply(iq IQ, label string, comp Component, appID string) error {
+	app, err := GetApplicationByPublicID(iq, appID)
+	if err != nil {
+		return fmt.Errorf("could not retrieve application with ID %s: %v", appID, err)
+	}
+
+	endpoint := fmt.Sprintf(restLabelComponent, comp.Hash, label, app.ID)
+	resp, err := iq.Del(endpoint)
+	if err != nil && resp.StatusCode != http.StatusNoContent {
+		return fmt.Errorf("could not unapply label: %v", err)
+	}
+
+	return nil
+}
---
+++ iq/componentLabels_test.go
@@ -0,0 +1,76 @@
+package nexusiq
+
+import (
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+)
+
+var dummyLabels = []string{"dummyLabel1", "dummyLabel2"}
+var testAppliedLabels = map[string]string{}
+
+func componentLabelsTestFunc(t *testing.T, w http.ResponseWriter, r *http.Request) {
+	switch {
+	case r.Method == http.MethodPost:
+		splitPath := strings.Split(r.URL.Path[1:], "/")
+		componentID, label, appID := splitPath[3], splitPath[5], splitPath[7]
+		key := fmt.Sprintf("%s:%s", componentID, appID)
+		testAppliedLabels[key] = label
+
+		w.WriteHeader(http.StatusNoContent)
+	case r.Method == http.MethodDelete:
+		splitPath := strings.Split(r.URL.Path[1:], "/")
+		componentID, label, appID := splitPath[3], splitPath[5], splitPath[7]
+		key := fmt.Sprintf("%s:%s", componentID, appID)
+
+		appliedLabel, ok := testAppliedLabels[key]
+		if !ok || appliedLabel != label {
+			w.WriteHeader(http.StatusNotFound)
+		} else {
+			delete(testAppliedLabels, key)
+
+			w.WriteHeader(http.StatusNoContent)
+		}
+	default:
+		w.WriteHeader(http.StatusMethodNotAllowed)
+	}
+}
+
+func componentLabelsTestIQ(t *testing.T) (iq IQ, mock *httptest.Server) {
+	return newTestIQ(t, func(t *testing.T, w http.ResponseWriter, r *http.Request) {
+		switch {
+		case strings.HasPrefix(r.URL.Path[1:], restApplication):
+			applicationTestFunc(t, w, r)
+		default:
+			componentLabelsTestFunc(t, w, r)
+		}
+	})
+}
+
+func TestComponentLabelApply(t *testing.T) {
+	iq, mock := componentLabelsTestIQ(t)
+	defer mock.Close()
+
+	label, component, appID := dummyLabels[0], dummyComponent, dummyApps[0].PublicID
+
+	if err := ComponentLabelApply(iq, label, component, appID); err != nil {
+		t.Error(err)
+	}
+}
+
+func TestComponentLabelUnapply(t *testing.T) {
+	iq, mock := componentLabelsTestIQ(t)
+	defer mock.Close()
+
+	label, component, appID := dummyLabels[0], dummyComponent, dummyApps[0].PublicID
+
+	if err := ComponentLabelApply(iq, label, component, appID); err != nil {
+		t.Fatal(err)
+	}
+
+	if err := ComponentLabelUnapply(iq, label, component, appID); err != nil {
+		t.Error(err)
+	}
+}
