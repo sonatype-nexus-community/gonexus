0416a4b66d175a4dd8c89edf0405a7a4317b18a1
HokieGeek <andres.f.perez@gmail.com>
2019-08-01 06:33:24 -0400

    [iq] Implementing data retention policy endpoint

--- iq/dataRetentionPolicies.go
+++ iq/dataRetentionPolicies.go
@@ -1,5 +1,11 @@
 package nexusiq
 
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+)
+
 const restDataRetentionPolicies = "api/v2/dataRetentionPolicies/organizations/%s"
 
 // DataRetentionPolicies encapsulates an organization's retention policies
@@ -8,9 +14,46 @@ type DataRetentionPolicies struct {
 	SuccessMetrics     DataRetentionPolicy `json:"successMetrics"`
 }
 
+// Equals compares two DataRetentionPolicies objects
+func (a *DataRetentionPolicies) Equals(b *DataRetentionPolicies) (_ bool) {
+	if a == b {
+		return true
+	}
+
+	if !a.ApplicationReports.Equals(&b.ApplicationReports) {
+		return
+	}
+
+	if !a.SuccessMetrics.Equals(&b.SuccessMetrics) {
+		return
+	}
+
+	return true
+}
+
 // ApplicationReports captures the policies related to application reports
 type ApplicationReports struct {
 	Stages map[Stage]DataRetentionPolicy `json:"stages"`
+}
+
+// Equals compares two ApplicationReports objects
+func (a *ApplicationReports) Equals(b *ApplicationReports) (_ bool) {
+	if a == b {
+		return true
+	}
+
+	if len(a.Stages) != len(b.Stages) {
+		return
+	}
+
+	for i, s := range a.Stages {
+		bs := b.Stages[i]
+		if !s.Equals(&bs) {
+			return
+		}
+	}
+
+	return true
 }
 
 // DataRetentionPolicy describes the retention policies for a pipeline stage
@@ -20,14 +63,64 @@ type DataRetentionPolicy struct {
 	MaxAge        string `json:"maxAge"`
 }
 
+// Equals compares two DataRetentionPolicy objects
+func (a *DataRetentionPolicy) Equals(b *DataRetentionPolicy) (_ bool) {
+	if a == b {
+		return true
+	}
+
+	if a.InheritPolicy != b.InheritPolicy {
+		return
+	}
+
+	if a.EnablePurging != b.EnablePurging {
+		return
+	}
+
+	if a.MaxAge != b.MaxAge {
+		return
+	}
+
+	return true
+}
+
 // GetRetentionPolicies returns the current retention policies
-func GetRetentionPolicies(iq IQ) (DataRetentionPolicies, error) {
-	//GET
-	return DataRetentionPolicies{}, nil
+func GetRetentionPolicies(iq IQ, orgName string) (policies DataRetentionPolicies, err error) {
+	org, err := GetOrganizationByName(iq, orgName)
+	if err != nil {
+		return policies, fmt.Errorf("could not retrieve organization named %s: %v", orgName, err)
+	}
+
+	endpoint := fmt.Sprintf(restDataRetentionPolicies, org.ID)
+
+	body, _, err := iq.Get(endpoint)
+	if err != nil {
+		return policies, fmt.Errorf("did not retrieve retention policies for organization %s: %v", orgName, err)
+	}
+
+	err = json.Unmarshal(body, &policies)
+
+	return
 }
 
 // SetRetentionPolicies updates the retention policies
-func SetRetentionPolicies(iq IQ, policies DataRetentionPolicies) error {
-	//PUT
+func SetRetentionPolicies(iq IQ, orgName string, policies DataRetentionPolicies) error {
+	org, err := GetOrganizationByName(iq, orgName)
+	if err != nil {
+		return fmt.Errorf("could not retrieve organization named %s: %v", orgName, err)
+	}
+
+	request, err := json.Marshal(policies)
+	if err != nil {
+		return fmt.Errorf("could not parse policies: %v", err)
+	}
+
+	endpoint := fmt.Sprintf(restDataRetentionPolicies, org.ID)
+
+	_, _, err = iq.Put(endpoint, bytes.NewBuffer(request))
+	if err != nil {
+		return fmt.Errorf("did not set retention policies for organization %s: %v", orgName, err)
+	}
+
 	return nil
 }
