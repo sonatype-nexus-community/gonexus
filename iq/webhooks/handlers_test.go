05551be205e357b13339cdc3e5eeca991bc35b63
HokieGeek <andres.f.perez@gmail.com>
2019-08-15 12:56:40 -0400

    [iq] Added remaining webhook event types

--- iq/webhooks/handlers_test.go
+++ iq/webhooks/handlers_test.go
@@ -30,13 +30,38 @@ func TestApplicationEvaluationEvents(t *testing.T) {
 	}
 }
 
+func TestViolationAlertEvents(t *testing.T) {
+	tests := []struct {
+		want   WebhookViolationAlert
+		closer bool
+	}{
+		{WebhookViolationAlert{Initiator: "dummy1"}, false},
+		{WebhookViolationAlert{Initiator: "dummy2"}, true},
+	}
+
+	for _, test := range tests {
+		events, close := ViolationAlertEvents()
+		if test.closer {
+			defer close()
+		}
+		sendViolationAlertEvent(test.want)
+
+		got := <-events
+		if !reflect.DeepEqual(got, test.want) {
+			t.Error("Did not get expected event")
+			t.Error("got", got)
+			t.Error("want", test.want)
+		}
+	}
+}
+
 func TestPolicyManagementEvents(t *testing.T) {
 	tests := []struct {
 		want   WebhookPolicyManagement
 		closer bool
 	}{
-		{WebhookPolicyManagement{}, false},
-		{WebhookPolicyManagement{}, true},
+		{WebhookPolicyManagement{policyOwner{ID: "dummy1", Name: "foobar1"}}, false},
+		{WebhookPolicyManagement{policyOwner{ID: "dummy2", Name: "foobar2"}}, true},
 	}
 
 	for _, test := range tests {
@@ -60,8 +85,8 @@ func TestLicenseOverrideEvents(t *testing.T) {
 		want   WebhookLicenseOverride
 		closer bool
 	}{
-		{WebhookLicenseOverride{}, false},
-		{WebhookLicenseOverride{}, true},
+		{WebhookLicenseOverride{licenseOverride{ID: "dummy1", OwnerID: "foobar1"}}, false},
+		{WebhookLicenseOverride{licenseOverride{ID: "dummy2", OwnerID: "foobar2"}}, true},
 	}
 
 	for _, test := range tests {
@@ -85,8 +110,8 @@ func TestSecurityOverrideEvents(t *testing.T) {
 		want   WebhookSecurityOverride
 		closer bool
 	}{
-		{WebhookSecurityOverride{}, false},
-		{WebhookSecurityOverride{}, true},
+		{WebhookSecurityOverride{securityVulnerabilityOverride{ID: "dummy1", OwnerID: "foobar1"}}, false},
+		{WebhookSecurityOverride{securityVulnerabilityOverride{ID: "dummy2", OwnerID: "foobar2"}}, true},
 	}
 
 	for _, test := range tests {
@@ -95,31 +120,6 @@ func TestSecurityOverrideEvents(t *testing.T) {
 			defer close()
 		}
 		sendSecurityOverrideEvent(test.want)
-
-		got := <-events
-		if !reflect.DeepEqual(got, test.want) {
-			t.Error("Did not get expected event")
-			t.Error("got", got)
-			t.Error("want", test.want)
-		}
-	}
-}
-
-func TestViolationAlertEvents(t *testing.T) {
-	tests := []struct {
-		want   WebhookViolationAlert
-		closer bool
-	}{
-		{WebhookViolationAlert{Initiator: "dummy1"}, false},
-		{WebhookViolationAlert{Initiator: "dummy2"}, true},
-	}
-
-	for _, test := range tests {
-		events, close := ViolationAlertEvents()
-		if test.closer {
-			defer close()
-		}
-		sendViolationAlertEvent(test.want)
 
 		got := <-events
 		if !reflect.DeepEqual(got, test.want) {
