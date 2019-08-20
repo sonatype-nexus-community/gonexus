32cdaf404675e75813ea033fb68a9ddcdd58d063
HokieGeek <andres.f.perez@gmail.com>
2019-08-20 13:59:49 -0400

    [iq] Added support for Success Metrics gathering

--- iq/reportMetrics.go
+++ iq/reportMetrics.go
@@ -1,35 +1,39 @@
 package nexusiq
 
 import (
+	"bytes"
+	"encoding/json"
+	"errors"
 	"fmt"
 	"time"
 )
 
 const restMetrics = "api/v2/reports/metrics"
 
+// MetricTimePeriod is the time period to use when analyzing the data
 type metricTimePeriod string
 
 const (
-	timePeriodMonthly metricTimePeriod = "MONTH"
-	timePeriodWeekly  metricTimePeriod = "WEEK"
+	metricsMonthly metricTimePeriod = "MONTH"
+	metricsWeekly  metricTimePeriod = "WEEK"
 )
 
 type metricRequest struct {
 	// "MONTH" or "WEEK"
-	TimePeriod metricTimePeriod `json:"timePeriod"`
+	TimePeriod metricTimePeriod `json:"timePeriod,omitempty"`
 
 	// If timePeriod is MONTH - an ISO 8601 year and month without timezone.
 	// If timePeriod is WEEK  - an ISO 8601 week year and week (e.g. week of 29 December 2008 is "2009-W01")
-	FirstTimePeriod string `json:"firstTimePeriod"`
+	FirstTimePeriod string `json:"firstTimePeriod,omitempty"`
 
 	// Same rules as above. Must be equal to or after firstTimePeriod. Can be omitted,
 	// in which case data for all successive time periods is provided including partial data for the current one.
-	LastTimePeriod string `json:"lastTimePeriod"`
+	LastTimePeriod string `json:"lastTimePeriod,omitempty"`
 
 	// If both of these are null or empty, data for all applications (that the user has access to) is returned.
 	// applicationIds are Internal ids.
-	ApplicationIDS  []string `json:"applicationIds"`
-	OrganizationIDS []string `json:"organizationIds"`
+	ApplicationIDS  []string `json:"applicationIds,omitempty"`
+	OrganizationIDS []string `json:"organizationIds,omitempty"`
 }
 
 type violationCountType string
@@ -52,56 +56,60 @@ type violationCounts struct {
 
 // Metrics encapsulates the data generate
 type Metrics struct {
-	ApplicationID       string        `json:"applicationId"`
-	ApplicationPublicID string        `json:"applicationPublicId"`
-	ApplicationName     string        `json:"applicationName"`
-	OrganizationID      string        `json:"organizationId"`
-	OrganizationName    string        `json:"organizationName"`
-	Aggregations        []aggregation `json:"aggregations"`
+	ApplicationID       string        `json:"applicationId,omitempty"`
+	ApplicationPublicID string        `json:"applicationPublicId,omitempty"`
+	ApplicationName     string        `json:"applicationName,omitempty"`
+	OrganizationID      string        `json:"organizationId,omitempty"`
+	OrganizationName    string        `json:"organizationName,omitempty"`
+	Aggregations        []aggregation `json:"aggregations,omitempty"`
 }
 
 type aggregation struct {
-	TimePeriodStart           string             `json:"timePeriodStart"`
-	EvaluationCount           int64              `json:"evaluationCount"`
-	MttrLowThreat             int64              `json:"mttrLowThreat"`
-	MttrModerateThreat        int64              `json:"mttrModerateThreat"`
-	MttrSevereThreat          int64              `json:"mttrSevereThreat"`
-	MttrCriticalThreat        int64              `json:"mttrCriticalThreat"`
-	DiscoveredCounts          violationCountsMap `json:"discoveredCounts"`
-	FixedCounts               violationCountsMap `json:"fixedCounts"`
-	WaivedCounts              violationCountsMap `json:"waivedCounts"`
-	OpenCountsAtTimePeriodEnd violationCountsMap `json:"openCountsAtTimePeriodEnd"`
+	TimePeriodStart           string             `json:"timePeriodStart,omitempty"`
+	EvaluationCount           int64              `json:"evaluationCount,omitempty"`
+	MttrLowThreat             int64              `json:"mttrLowThreat,omitempty"`
+	MttrModerateThreat        int64              `json:"mttrModerateThreat,omitempty"`
+	MttrSevereThreat          int64              `json:"mttrSevereThreat,omitempty"`
+	MttrCriticalThreat        int64              `json:"mttrCriticalThreat,omitempty"`
+	DiscoveredCounts          violationCountsMap `json:"discoveredCounts,omitempty"`
+	FixedCounts               violationCountsMap `json:"fixedCounts,omitempty"`
+	WaivedCounts              violationCountsMap `json:"waivedCounts,omitempty"`
+	OpenCountsAtTimePeriodEnd violationCountsMap `json:"openCountsAtTimePeriodEnd,omitempty"`
 }
-
-// TODO: Accept header: application/json or text/csv
 
 // MetricsRequestBuilder builds a request to retrieve metrics data from IQ
 type MetricsRequestBuilder struct {
-	timePeriod                      string
+	timePeriod                      metricTimePeriod
 	firstTimePeriod, lastTimePeriod time.Time
 	apps, orgs                      []string
 }
 
-// TimePeriod allows you to set the time period type. Defaults to MONTH
-func (b *MetricsRequestBuilder) TimePeriod(v string) *MetricsRequestBuilder {
-	b.timePeriod = v
+// Monthly sets the request to use a monthly time period
+func (b *MetricsRequestBuilder) Monthly() *MetricsRequestBuilder {
+	b.timePeriod = metricsMonthly
 	return b
 }
 
-// FirstTimePeriod allows you to set the starting time period for the data gathering
-func (b *MetricsRequestBuilder) FirstTimePeriod(v time.Time) *MetricsRequestBuilder {
+// Weekly sets the request to use a weekly time period
+func (b *MetricsRequestBuilder) Weekly() *MetricsRequestBuilder {
+	b.timePeriod = metricsWeekly
+	return b
+}
+
+// StartingOn allows you to set the starting time period for the data gathering
+func (b *MetricsRequestBuilder) StartingOn(v time.Time) *MetricsRequestBuilder {
 	b.firstTimePeriod = v
 	return b
 }
 
-// LastTimePeriod allows you to set the ending time period for the data gathering. Optional
-func (b *MetricsRequestBuilder) LastTimePeriod(v time.Time) *MetricsRequestBuilder {
+// EndingOn allows you to set the ending time period for the data gathering. Optional
+func (b *MetricsRequestBuilder) EndingOn(v time.Time) *MetricsRequestBuilder {
 	b.lastTimePeriod = v
 	return b
 }
 
-// Application adds an application whose data to include
-func (b *MetricsRequestBuilder) Application(v string) *MetricsRequestBuilder {
+// WithApplication adds an application (by Public ID) whose data to include
+func (b *MetricsRequestBuilder) WithApplication(v string) *MetricsRequestBuilder {
 	if b.apps == nil {
 		b.apps = make([]string, 0)
 	}
@@ -109,8 +117,8 @@ func (b *MetricsRequestBuilder) Application(v string) *MetricsRequestBuilder {
 	return b
 }
 
-// Organization adds an application whose data to include
-func (b *MetricsRequestBuilder) Organization(v string) *MetricsRequestBuilder {
+// WithOrganization adds an organization whose data to include in†he report
+func (b *MetricsRequestBuilder) WithOrganization(v string) *MetricsRequestBuilder {
 	if b.orgs == nil {
 		b.orgs = make([]string, 0)
 	}
@@ -118,37 +126,94 @@ func (b *MetricsRequestBuilder) Organization(v string) *MetricsRequestBuilder {
 	return b
 }
 
-func (b *MetricsRequestBuilder) build() (req metricRequest) {
+func (b *MetricsRequestBuilder) build(iq IQ) (req metricRequest, err error) {
 	// If timePeriod is MONTH - an ISO 8601 year and month without timezone.
 	// If timePeriod is WEEK  - an ISO 8601 week year and week (e.g. week of 29 December 2008 is "2009-W01")
 	formatTime := func(t time.Time) string {
 		switch req.TimePeriod {
-		case timePeriodWeekly:
+		case metricsWeekly:
 			y, w := t.ISOWeek()
 			return fmt.Sprintf("%d-W%02d", y, w)
-		case timePeriodMonthly:
+		case metricsMonthly:
 			fallthrough
 		default:
 			return t.Format("2006-01")
 		}
 	}
 
-	req.TimePeriod = timePeriodMonthly
+	req.TimePeriod = metricsMonthly // TODO: validate
 	if b.timePeriod != "" {
-		req.TimePeriod = metricTimePeriod(b.timePeriod)
+		req.TimePeriod = b.timePeriod
+	}
+
+	// TODO: would be nice to not have to require this
+	if b.firstTimePeriod.IsZero() {
+		return req, errors.New("a starting time period is required")
 	}
 	req.FirstTimePeriod = formatTime(b.firstTimePeriod)
+
 	if !b.lastTimePeriod.IsZero() {
+		// Must be equal to or after firstTimePeriod
+		if b.lastTimePeriod.Before(b.firstTimePeriod) {
+			return req, errors.New("ending time period must be equal to, or after, the starting time period")
+		}
 		req.LastTimePeriod = formatTime(b.lastTimePeriod)
 	}
 
-	req.ApplicationIDS = b.apps
-	req.OrganizationIDS = b.orgs
+	if b.apps != nil {
+		req.ApplicationIDS = make([]string, len(b.apps))
+		for i, a := range b.apps {
+			app, er := GetApplicationByPublicID(iq, a)
+			if er != nil {
+				return req, fmt.Errorf("could not find application with public id %s: %v", a, er)
+			}
+			req.ApplicationIDS[i] = app.ID
+		}
+	}
+
+	if b.orgs != nil {
+		req.OrganizationIDS = make([]string, len(b.orgs))
+		for i, o := range b.orgs {
+			org, er := GetOrganizationByName(iq, o)
+			if er != nil {
+				return req, fmt.Errorf("could not find organization with name %s: %v", o, er)
+			}
+			req.OrganizationIDS[i] = org.ID
+		}
+	}
 
 	return
 }
 
+// NewMetricsRequestBuilder returns a new builder instance
+func NewMetricsRequestBuilder() *MetricsRequestBuilder {
+	return new(MetricsRequestBuilder)
+}
+
 // GenerateMetrics creates metrics from the given qualifiers
-func GenerateMetrics(iq IQ, builder MetricsRequestBuilder) Metrics {
-	return Metrics{}
+func GenerateMetrics(iq IQ, builder *MetricsRequestBuilder) ([]Metrics, error) {
+	// TODO: Accept header: application/json or text/csv
+
+	req, err := builder.build(iq)
+	if err != nil {
+		return nil, fmt.Errorf("could not build request: %v", err)
+	}
+
+	buf, err := json.Marshal(req)
+	if err != nil {
+		return nil, fmt.Errorf("could not marshal request: %v", err)
+	}
+
+	body, _, err := iq.Post(restMetrics, bytes.NewBuffer(buf))
+	if err != nil {
+		return nil, fmt.Errorf("could not issue request to IQ: %v", err)
+	}
+
+	var metrics []Metrics
+	err = json.Unmarshal(body, &metrics)
+	if err != nil {
+		return nil, fmt.Errorf("could not read response from IQ: %v", err)
+	}
+
+	return metrics, nil
 }
